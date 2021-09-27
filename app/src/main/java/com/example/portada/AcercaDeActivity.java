1199779c979a847524e402640a0cca4d60e5ae42
angelcan58 <angelcan58@gmail.com>
2021-09-27 18:53:40 -0500

    first commit

---
+++ app/src/main/java/com/example/portada/AcercaDeActivity.java
@@ -0,0 +1,17 @@
+package com.example.portada;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class AcercaDeActivity extends AppCompatActivity {
+
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade);
+
+    }
+}
